7279c506d4dd119a0f9d7221a57e11f1dbe01a2e
Mohammad Morovati (College of DuPage) <morovatm@cod.edu>
2025-08-01 13:55:23 -0500

    Update echoBot.py

--- echoBot.py
+++ echoBot.py
@@ -4,10 +4,10 @@ st.title("Echo Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
-st.session_state.messages = []
+  st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-with st.chat_message(message["role"]):
-st.markdown(message["content"])
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
 
