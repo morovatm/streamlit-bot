0b436f91d04c5203a2728362793b89e911aac53e
Mohammad Morovati (College of DuPage) <morovatm@cod.edu>
2025-08-01 15:06:48 -0500

    Update AI-Bot.py

--- AI-Bot.py
+++ AI-Bot.py
@@ -81,7 +81,7 @@ st.session_state.messages.append({"role": "user", "content": prompt})
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-response = st.write_stream(response_generator())
+  response = st.write_stream(response_generator())
 
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
